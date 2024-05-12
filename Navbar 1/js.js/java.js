34d990172bc6e7835657dcff92d05a84d89fd29c
dharani2812 <121165539+dharani2812@users.noreply.github.com>
2024-05-12 17:26:10 +0530

    Add files via upload

---
+++ Navbar 1/js.js/java.js
@@ -0,0 +1,11 @@
+var sidenav=document.querySelector(".side-navbar")
+var blurcontent=document.querySelector(".content")
+
+
+function shownav(){
+    sidenav.style.left="0"
+}
+function closenav(){
+    sidenav.style.left="-30%"
+}
+
